ae7474b9b52c3634b9d7edc2f77448e0dcc5a28e
kou0123 <kounetinfo@gmail.com>
2019-01-03 21:19:02 +0900

    first commit

---
+++ project/manager/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView
+
+from manager.models import *
+
+class WorkerListView(TemplateView):
+    template_name = "worker_list.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(WorkerListView, self).get_context_data(**kwargs)
+
+        workers = Worker.objects.all()  # データベースからオブジェクトを取得して
+        context['workers'] = workers  # 入れ物に入れる
+
+        return render(self.request, self.template_name, context)
